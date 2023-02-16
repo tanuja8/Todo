23c7cd0dd4a59f83f55fd3ca2bab52897affe2af
Tanuja <tanujakhatun292@gmail.com>
2023-02-16 23:53:52 +0530

    project update

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -17,10 +17,10 @@ export default function Todo() {
   }
   return (
     <div className="todolist mt-5">
-      <div className=' container'>
          <h2 className="text-white m-3">What's the plan for Today?</h2>
-         <input type="text" placeholder='Add  your Activity...' value={activity} onChange={(e)=>setActivity(e.target.value)}/>
-         <button onClick={addAtivity}>Add</button>
+         <div className=" py-3">
+         <input type="text" placeholder='Enter your Activity...' value={activity} onChange={(e)=>setActivity(e.target.value)}/>
+         <button type="button" className="addbtn px-2 mx-3 text-white"  onClick={addAtivity}>Add Task</button>
          </div>
          <div>
           {listData.map(data=>{
