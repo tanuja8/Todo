b27701e5f7fdd1705f2edb1ae10bd93d753a6689
Tanuja <tanujakhatun292@gmail.com>
2023-03-05 02:33:42 +0530

    project updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Todo from './Component/Todo';
-import Task from './Component/Task';
 
 
 function App() {
   return (
     <div className="App">
     <Todo></Todo>  
-    <Task></Task>
     </div>
   );
 }
--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -21,10 +21,13 @@ const updatedListData=listData.filter((element,id)=>{
 })
 setlistData(updatedListData);
 }
+function removeall(){
+  setlistData([])
+}
   return (
     <div className="todolist mt-5">
          <h2 className="text-white m-3">What's the plan for Today?</h2>
-         <div className=" py-3">
+         <div className=" py-4 mb-2">
          <input type="text" placeholder='Enter your Activity...' value={activity} onChange={(e)=>setActivity(e.target.value)}/>
          <button type="button" className="addbtn px-2 mx-3 text-white"  onClick={addAtivity}>Add Task</button>
          </div>
@@ -32,17 +35,18 @@ setlistData(updatedListData);
           {listData.map((data,i)=>{
             return(
             <p key={i}>   
-              <div className="py-3 ">
+              <div className="py-2">
              <div className='text-white  task_area'>
                <input type="text" value={data }></input>
               <button className="R_btn px-2" onClick={()=>removeActivity(i)}>Remove</button>
-  
               </div>
               </div>
               </p>
             )
           })}
-         
+         <div className="m-2 py-3">
+              {listData.length >=1 &&  <button className="R_btn px-2 " onClick={removeall}>Remove All</button> }
+              </div>
          </div>
      
     </div>
